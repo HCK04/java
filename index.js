5fc2bef3bcd99fb7f005a890041cf99b888bb884
HCK04 <meganx6s@gmail.com>
2024-01-30 09:37:25 +0100

    java

---
+++ index.js
@@ -0,0 +1,55 @@
+// console.log("Hello World")
+// alert("ALERT!!!!!")
+
+// function greet(name) {
+//   console.log("Hello " + name);
+// }
+
+// function somme(x, y) {
+// 	return x + y
+// }
+// console.log(somme(13, 80))
+
+// // declaration des var
+// const PI = 3.14
+// function (inscrire); {
+// 	let nom;
+// 	let prenom;
+// 	let age;
+// 	nom ="Maih"
+// 	prenom = "Akram"
+// 	age = 19
+// 	if (age < 22){
+// 		var statut;       //let
+// 		statut = "stagiaire"
+// 	}
+// 	console.log(nom);
+// 	console.log(status);
+// }
+
+// let text1= "AKRAM"
+// text1 += "";
+// let text2 = "en force";
+// let text3 = text1 + text2;
+
+// let a = 5
+// let b = '5'
+
+// if (a == b){
+// 	alert("YES")
+// }
+// if(a === b){
+// 	alert("NO")
+
+// }
+
+let x = 5
+let y = '8'
+if (x == 5 && y === 10) {
+	console.log("TRUE")
+	
+}else{
+	console.log("FALSE")
+}
+
+// JAVASCRIPT TUTORIAL!!! 
