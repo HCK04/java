c036b4f73da048c5733f16eb7e67e3ea5fc72cd1
HCK04 <meganx6s@gmail.com>
2024-02-13 10:40:08 +0100

    Java

---
+++ Session-3/index.js
@@ -0,0 +1,36 @@
+console.log(typeof("to be or not to be"))
+
+console.log(typeof(undefined))
+
+console.log(typeof(true))
+
+// Passage par valeur //
+let a;
+let b;
+a = 5;
+b = a;
+b = 6;
+
+console.log(a)
+console.log(b)
+
+
+// Types des objets de base //
+
+liste = [1, 2, 3]
+
+clist = liste
+
+clist.push(5)
+
+console.log(liste)
+
+// stagiaire = {
+// 	id : 1
+// 	prenom : "Akram"
+// }
+
+// cstg = stagiaire
+// cstg.age = 14
+// console.log(stagiaire)
+// console.log(cstg)
