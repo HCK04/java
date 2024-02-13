8ea8fa311810bb2265456486da3b5364fbaa6524
HCK04 <meganx6s@gmail.com>
2024-02-13 12:07:14 +0100

    s7

--- session-7/index-7.js
+++ session-7/index-7.js
@@ -13,10 +13,10 @@ function sommeCalcule(){
 
 
   if (somme > 100){
-    document.body.style.backgroundColor = "red";
+    document.querySelector(".age2").style.backgroundColor = "red";
   }
   else{
-  document.body.style.backgroundColor = "green";
+    document.querySelector(".age").style.backgroundColor = "green";
   }
 
 
@@ -25,8 +25,63 @@ function sommeCalcule(){
 sommeCalcule();
 
 
-// let y = x.value
+// -------------------------------------------------------------------------------------------------------------------
 
-// let z = x + 10;
+// Button click
 
-// console.log(z);
+function BackgroundColor() {
+  document.querySelector(".age").style.backgroundColor = "red";
+}
+
+document.querySelector(".btn").addEventListener("click", BackgroundColor); //NO "()" SO IT DOESNT EXECUTE BY ITSELF
+
+
+// CALCULE
+
+document.querySelector(".btn").addEventListener("click", () => {
+  let x = +document.querySelector(".age").value;
+  console.log(x);
+  let y = +document.querySelector(".age2").value;
+  console.log(y);
+
+  let z = x + y;
+  document.querySelector(".age3").value = z;
+
+  if (z > 100) {
+    document.querySelector(".age3").style.backgroundColor = "red";
+  } else {
+    document.querySelector(".age3").style.backgroundColor = "green";
+  }
+});
+
+
+// RESET 
+
+document.querySelector(".reset").addEventListener("click", () => {
+  document.querySelector(".age").value = "";
+  document.querySelector(".age2").value = "";
+  document.querySelector(".age3").value = "";
+  document.querySelector(".age").style.backgroundColor = "";
+  console.log("reset clicked")
+})
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
